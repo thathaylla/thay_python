2f48d05518d5cb7a3ad5d4239477a8ef5087688e
Thaylla De Marco <thaylla.marco@gmail.com>
2024-02-23 17:25:51 -0300

    Calculadora de semanas de vida

---
+++ calc_semanas_vida.py
@@ -0,0 +1,5 @@
+#calculo semanas baseado no livro que diz que temos 4.000 semanas de vida
+idade = input("Qual a sua idade?")
+idade_int = 90 - int(idade)
+semanas = idade_int * 52
+print(f"Você tem {semanas} semanas de vida!")
