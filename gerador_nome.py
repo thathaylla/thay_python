c341539138d7811291beda37c30c1c5023eecc5b
Thaylla De Marco <thaylla.marco@gmail.com>
2024-02-23 15:53:54 -0300

    #Gerardor de nome de banda combinando nome da cidade + nome do pet

---
+++ gerador_nome.py
@@ -0,0 +1,7 @@
+#Gerardor de nome de banda combinando nome da cidade + nome do pet
+print("Bem vindo ao Gerador de Nomes de Bandas!!")
+
+cidade = input("Qual cidade você nasceu?\n")
+pet = input("Qual o nome do seu pet?\n")
+
+print("Sua banda pode se chamar: " + cidade + " " + pet)
